e1a7ab632b15ad84acad39a02698f44eaf698b16
George Barrett <georgeaustinbarrett@gmail.com>
2023-05-20 17:21:19 +0100

    class and methods outlined

---
+++ lib/bowling.rb
@@ -0,0 +1,27 @@
+class Bowling
+
+  def initialize
+  end
+
+  def roll(pins)
+  end
+
+  def score
+  end
+
+  def strike(frame)
+  end
+
+  def sum_of_points_in_frame(frame)
+  end
+
+  def spare_bonus(frame)
+  end
+
+  def strike_bonus(frame)
+  end
+
+  def spare(frame)
+  end
+
+end
